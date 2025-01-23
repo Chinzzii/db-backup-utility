76ed6bcc9b402939208bc86945adebf0288b03ff
Chinzzii <chinmay.singhania@gmail.com>
2025-01-22 19:52:20 -0500

    Added package

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "github.com/Chinzzii/db-backup-utility/internal/backup"
+    "github.com/Chinzzii/db-backup-utility/internal/database"
+    "github.com/Chinzzii/db-backup-utility/internal/storage"
+    "github.com/Chinzzii/db-backup-utility/internal/logger"
+)
